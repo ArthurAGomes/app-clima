d3c1c1cbbfd9d973a4dac89ab30f490e4aaf4466
ArthurAgomes <arthur.agomes06@gmail.com>
2024-08-02 19:59:56 -0300

    maximo e minimo

--- src/components/weatherinfo/index.jsx
+++ src/components/weatherinfo/index.jsx
@@ -1,6 +1,6 @@
+// Weatherinfo.js
 import React from 'react';
 import "./tempoatual.css"
-
 
 function Weatherinfo({ weather }) {
     if (!weather || !weather.weather || !weather.weather[0]) {
@@ -10,19 +10,26 @@ function Weatherinfo({ weather }) {
     const iconCode = weather.weather[0].icon;
     const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
 
-
     return (
         <div className='containerweather'>
             <h2>{weather.name}</h2>
             <div className='weather-info'>
                 <img src={iconUrl} alt="Weather icon"/>
-                <p className='temp'>{Math.round(weather.main.temp)}°C</p>
+                <p className='temp'>Temperatura Atual: {Math.round(weather.main.temp)}°C</p>
+                <p className='temp-max'>Temperatura Máxima: {Math.round(weather.main.temp_max)}°C</p>
+                <p className='temp-min'>Temperatura Mínima: {Math.round(weather.main.temp_min)}°C</p>
+                <p className='descricption'>{weather.weather[0].description}</p>
             </div>
-            <p className='descricption'>{weather.weather[0].description}</p>
             <div className='details'>
-                <p>Sensação térmica: {Math.round(weather.main.feels_like)}°C</p>
-                <p>Umidade: {weather.main.humidity}%</p>
-                <p>Pressão: {weather.main.pressure}</p>
+                <div>
+                    <p>Sensação térmica: {Math.round(weather.main.feels_like)}°C</p>
+                </div>
+                <div>
+                    <p>Umidade: {weather.main.humidity}%</p>
+                </div>
+                <div>
+                    <p>Pressão: {weather.main.pressure} hPa</p>
+                </div>
             </div>
         </div>
     );
